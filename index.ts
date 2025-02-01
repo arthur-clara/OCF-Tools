7892fd4a0ec959c3e5e020ed9c9c9aadbcb7f938
Matthew Cantor <matthewcantor@gmail.com>
2025-02-01 10:27:03 -0500

    conforming main index.ts file to vesting_schedule_generator_v1

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import { readOcfPackage } from "./read_ocf_package";
-import { generateSchedule } from "./vesting_schedule_generator";
-import { vestingStatusCheck } from "./vesting_status_check";
-import { isoNsoCalculator } from "./iso_nso_calculator";
+import { VestingScheduleGenerator } from "vesting_schedule_generator_v1";
+import { ISOCalculator } from "iso_nso_calculator";
 import { ocfValidator } from "./ocf_validator";
 import { ocfSnapshot } from "./ocf_snapshot";
 
-module.exports = { readOcfPackage, generateSchedule, vestingStatusCheck, isoNsoCalculator, ocfValidator, ocfSnapshot };
+module.exports = {
+  readOcfPackage,
+  VestingScheduleGenerator,
+  ISOCalculator,
+  ocfValidator,
+  ocfSnapshot,
+};
